134a9a7c0aeb826bd6850b6392b8e16c5592b866
William Peroche <william.peroche@asb.co.nz>
2018-06-26 10:53:33 +1200

    Some changes

--- app/src/main/java/com/example/myapplication/androidtuts/MainActivity.java
+++ app/src/main/java/com/example/myapplication/androidtuts/MainActivity.java
@@ -1,13 +1,86 @@
 package com.example.myapplication.androidtuts;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    String msg = "Android : ";
+    EditText e;
 
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+
+        e = findViewById(R.id.editText2);
+        e.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    Log.d(msg, "NotFocus");
+                } else {
+                    Log.d(msg, "Focus");
+                }
+            }
+        });
+    }
+
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    // broadcast a custom intent.
+    public void broadcastIntent(View view){
+        Intent intent = new Intent();
+        intent.setAction("com.example.myapplication.androidtuts.CUSTOM_INTENT");
+        sendBroadcast(intent);
+    }
+
+    public void onClickAddName(View view) {
+        // Add a new student record
+        ContentValues values = new ContentValues();
+        values.put(StudentsProvider.NAME,
+                ((EditText)findViewById(R.id.editText2)).getText().toString());
+
+        values.put(StudentsProvider.GRADE,
+                ((EditText)findViewById(R.id.editText3)).getText().toString());
+
+        Uri uri = getContentResolver().insert(
+                StudentsProvider.CONTENT_URI, values);
+
+        Toast.makeText(getBaseContext(),
+                uri.toString(), Toast.LENGTH_LONG).show();
+    }
+    public void onClickRetrieveStudents(View view) {
+        // Retrieve student records
+        String URL = "content://com.example.MyApplication.StudentsProvider";
+
+        Uri students = Uri.parse(URL);
+        Cursor c = managedQuery(students, null, null, null, "name");
+
+        if (c.moveToFirst()) {
+            do{
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(StudentsProvider._ID)) +
+                                ", " +  c.getString(c.getColumnIndex( StudentsProvider.NAME)) +
+                                ", " + c.getString(c.getColumnIndex( StudentsProvider.GRADE)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
     }
 }
